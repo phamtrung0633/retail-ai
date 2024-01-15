5f5ab90cb639e354fecc1d77698f6ae72f3e853c
Phillip Virgona <p.j.virgona@gmail.com>
2024-01-15 12:44:10 +1000

    Add https://github.com/hi-speedroid/product-embeddings @ 4d656de ["Fix schema imports"]

---
+++ 3D-tracking/embeddings/schema.py
@@ -0,0 +1,35 @@
+from pymilvus import CollectionSchema, FieldSchema, DataType
+
+FIELDS = {
+    'VECTOR': 'embedding',
+    'SKU': 'sku'
+}
+
+def create_schema(dim, model):
+    return CollectionSchema(
+        fields = [
+            FieldSchema(
+                name = 'id',
+                dtype = DataType.INT64,
+                is_primary = True
+            ),
+            FieldSchema(
+                name = FIELDS['SKU'],
+                dtype = DataType.VARCHAR,
+                max_length = 64
+            ),
+            FieldSchema(
+                name = FIELDS['VECTOR'],
+                dtype = DataType.FLOAT_VECTOR,
+                dim = dim
+            )
+        ],
+        description = f'Product embeddings and associated SKUs ({model})',
+        enable_dynamic_field = True,
+        auto_id = True,
+    )
+
+Params = {
+    'metric_type': 'COSINE',
+    'index_type': 'FLAT'
+}
