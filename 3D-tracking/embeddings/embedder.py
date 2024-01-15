6b02b45c15eebe522eccc2cf4776b45988aca2fa
Phillip Virgona <p.j.virgona@gmail.com>
2024-01-15 20:12:38 +1100

    Move Milvus initialisation to non global scope

--- 3D-tracking/embeddings/embedder.py
+++ 3D-tracking/embeddings/embedder.py
@@ -14,81 +14,11 @@ from torchvision import models, transforms
 from torchvision.models import feature_extraction
 from pymilvus import Collection, Partition, MilvusClient, connections
 
-dotenv.load_dotenv()
 
 NUM_CHANNELS = 3
 PRECLASSIFICATION_IDX = -2
 
 PARAMS_FILE = 'params.yaml'
-
-module = os.path.dirname(__file__)
-
-params = dvc.api.params_show(os.path.join(module, PARAMS_FILE))
-path = os.path.join(module, params['model']['path'], params['model']['name'])
-config_artifact = os.path.join(module, params['config'])
-
-print(path)
-
-if not (os.path.exists(path) and os.path.exists(config_artifact)):
-    print("Artifacts from stage 'prepare' are missing, run `dvc repro` in the containing directory to restore them.")
-    sys.exit()
-
-with open(config_artifact) as config:
-    arch = yaml.load(config.read(), Loader = yaml.Loader)['arch']
-    print(f"Model architecture '{arch}' detected")
-
-model = models.get_model(arch, weights = None)
-weights = models.get_model_weights(arch).DEFAULT
-
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
-
-model.load_state_dict(torch.load(path))
-model = model.to(device)
-
-model.eval()
-
-preprocess = transforms.Compose([
-    transforms.ToTensor(),
-    weights.transforms(antialias = True),
-])
-
-train_nodes, eval_nodes = models.feature_extraction.get_graph_node_names(model)
-preclassifier = eval_nodes[PRECLASSIFICATION_IDX]
-
-fx = models.feature_extraction.create_feature_extractor(model, return_nodes = [preclassifier])
-
-print(f"Feature extractor traced to layer '{preclassifier}'")
-print("Tracing model execution...")
-
-with torch.no_grad():
-    dummy = preprocess(zeros((1, 1, NUM_CHANNELS)))
-    output = fx(dummy.unsqueeze(0).to(device))[preclassifier].squeeze(0)
-
-    print(f"\tIn Dims: {list(dummy.shape)}")
-    print(f"\tOut Dims: {list(output.shape)}\n")
-
-milvus = MilvusClient(
-    uri = os.getenv('MILVUS_URI'),
-    token = os.getenv('MILVUS_TOKEN')
-)
-
-context = next(iter(connections._connected_alias))
-products = os.getenv('PRODUCTS_COLLECTION')
-
-if products not in milvus.list_collections():
-    products = Collection(products, schema = schema.create_schema(dim = output.shape[0], model = arch), using = context)
-    products.create_index(
-        schema.FIELDS['VECTOR'],
-        schema.Params
-    )
-else:
-    products = Collection(products, using = context)
-
-products.load()
-print(f"Collection '{products.name}' loaded")
 
 class Embedder:
 
@@ -98,15 +28,99 @@ class Embedder:
         self._collection = collection
         self._partitions = {}
 
+        self.products = None
+
+        self.preclassifier = None
+        self.preprocess = None
+        self.fx = None
+
+    def initialise(self):
+        dotenv.load_dotenv()
+
+        module = os.path.dirname(__file__)
+
+        params = dvc.api.params_show(os.path.join(module, PARAMS_FILE))
+        path = os.path.join(module, params['model']['path'], params['model']['name'])
+        config_artifact = os.path.join(module, params['config'])
+
+        print(path)
+
+        if not (os.path.exists(path) and os.path.exists(config_artifact)):
+            print("Artifacts from stage 'prepare' are missing, run `dvc repro` in the containing directory to restore them.")
+            sys.exit()
+
+        with open(config_artifact) as config:
+            arch = yaml.load(config.read(), Loader = yaml.Loader)['arch']
+            print(f"Model architecture '{arch}' detected")
+
+        model = models.get_model(arch, weights = None)
+        weights = models.get_model_weights(arch).DEFAULT
+
+        if torch.cuda.is_available():
+            device = torch.device('cuda')
+        else:
+            device = torch.device('cpu')
+
+        model.load_state_dict(torch.load(path))
+        model = model.to(device)
+
+        model.eval()
+
+        preprocess = transforms.Compose([
+            transforms.ToTensor(),
+            weights.transforms(antialias = True),
+        ])
+
+        train_nodes, eval_nodes = models.feature_extraction.get_graph_node_names(model)
+        preclassifier = eval_nodes[PRECLASSIFICATION_IDX]
+
+        fx = models.feature_extraction.create_feature_extractor(model, return_nodes = [preclassifier])
+
+        print(f"Feature extractor traced to layer '{preclassifier}'")
+        print("Tracing model execution...")
+
+        with torch.no_grad():
+            dummy = preprocess(zeros((1, 1, NUM_CHANNELS)))
+            output = fx(dummy.unsqueeze(0).to(device))[preclassifier].squeeze(0)
+
+            print(f"\tIn Dims: {list(dummy.shape)}")
+            print(f"\tOut Dims: {list(output.shape)}\n")
+
+        milvus = MilvusClient(
+            uri = os.getenv('MILVUS_URI'),
+            token = os.getenv('MILVUS_TOKEN')
+        )
+
+        context = next(iter(connections._connected_alias))
+        products = os.getenv('PRODUCTS_COLLECTION')
+
+        if products not in milvus.list_collections():
+            products = Collection(products, schema = schema.create_schema(dim = output.shape[0], model = arch), using = context)
+            products.create_index(
+                schema.FIELDS['VECTOR'],
+                schema.Params
+            )
+        else:
+            products = Collection(products, using = context)
+
+        products.load()
+        print(f"Collection '{products.name}' loaded")
+
+        self.products = products
+
+        self.preclassifier = preclassifier
+        self.preprocess = preprocess
+        self.fx = fx
+
     def _vectorize(self, image):
         with torch.no_grad():
-            batch = preprocess(image).unsqueeze(0).to(device)
-            return fx(batch)[preclassifier].squeeze(0)
+            batch = self.preprocess(image).unsqueeze(0).to(device)
+            return self.fx(batch)[self.preclassifier].squeeze(0)
 
     def _vectorize_many(self, images):
         with torch.no_grad():
-            batch = torch.stack(*[preprocess(image) for image in images]).to(device)
-            return fx(batch)[preclassifier]
+            batch = torch.stack(*[self.preprocess(image) for image in images]).to(device)
+            return self.fx(batch)[self.preclassifier]
     
     def _get_partition(self, shelf, create_on_missing = False):
         partition = self._partitions.get(shelf)
