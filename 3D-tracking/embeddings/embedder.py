bd9cd1548a4b51e732ec27ba390fa116fabe6984
Phillip Virgona <p.j.virgona@gmail.com>
2024-01-16 21:26:33 +1100

    Fixed image stacking in insert_many

--- 3D-tracking/embeddings/embedder.py
+++ 3D-tracking/embeddings/embedder.py
@@ -117,7 +117,7 @@ class Embedder:
 
     def _vectorize_many(self, images):
         with torch.no_grad():
-            batch = torch.stack(*[self.preprocess(image) for image in images]).to(device)
+            batch = torch.stack([self.preprocess(image) for image in images]).to(device)
             return self.fx(batch)[self.preclassifier]
     
     def _get_partition(self, shelf, create_on_missing = False):
