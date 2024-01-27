1bd3df6ec99794ca0cc8a496ccfcba8c0a49b838
Victor Pham <phamtrung0633@gmail.com>
2024-01-28 01:25:12 +1000

    Added code for improved shoppers behavior analysis to allow multiple actions within a proximity event, and also the ability to use foot joints to check proximity when hand joint is not visible - Needed testing of both functionality

--- 3D-tracking/test.py
+++ 3D-tracking/test.py
@@ -1,6 +1,18 @@
-from embeddings.embedder import Embedder
-import cv2
-e = Embedder()
-e.initialise()
-m = cv2.imread("images/environmentRight/2.png")
-e.search("shelf_1", m)
+import numpy as np
+from scipy.optimize import linear_sum_assignment
+
+# Assuming you have a matrix A(N, M)
+# Replace this with your actual matrix
+A = np.array([[5, 9, 1],
+              [10, 3, 2],
+              [8, 7, 4]])
+
+# Apply the Hungarian algorithm to find the assignment
+row_ind, col_ind = linear_sum_assignment(A, maximize=True)
+
+# Sort the assignments based on the values in the matrix
+sorted_indices = np.argsort(-A[row_ind, col_ind])
+
+# Get the top 5 pairs
+for i, j in zip(row_ind[sorted_indices][:2], col_ind[sorted_indices][:2]):
+    print(i, j)
