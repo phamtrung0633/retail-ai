49f478dd6daf188dc49ef178235236e5d1bdc534
Phillip Virgona <p.j.virgona@gmail.com>
2024-01-31 20:38:42 +1100

    Add hand segmentor

---
+++ 3D-tracking/transforms/hands/hands.py
@@ -0,0 +1,57 @@
+from transforms import Transform, device
+from transforms.hands.model import HandSegModel
+
+import os
+
+import torch
+import torch.nn.functional as F
+
+from torchvision import transforms
+
+import numpy as np
+import cv2
+
+class HandSegmentor(Transform):
+    _pool = None
+    _count = 0
+
+    def __init__(self, out_dims, map_size):
+        super().__init__()
+
+        self.resize = transforms.Resize(out_dims[::-1])
+
+        self.preprocess = transforms.Compose([
+            transforms.ToTensor(),
+            self.resize,
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.Resize(map_size[::-1])
+        ])
+
+    def is_initialised(self):
+        return self.cls()._pool is not None
+
+    def initialise(self):
+        module = os.path.dirname(__file__)
+        weights = 'weights/hands.ckpt'
+
+        options = {'pretrained': True}
+
+        model = HandSegModel.load_from_checkpoint(os.path.join(module, weights), **options)
+        model.eval()
+
+        model.to(device)
+
+        self.set_pool(model)
+
+    def forward(self, image):
+        model = self.get_pool()
+
+        with torch.no_grad():
+            batch = self.preprocess(image).unsqueeze(0).to(device)
+            logits = model(batch).cpu()
+
+            preds = (F.softmax(logits, 1).argmax(1)[0] * 255).unsqueeze(0)
+            mask = self.resize(preds).squeeze(0).numpy().astype(np.uint8)
+
+        mask = cv2.bitwise_not(mask)
+        return cv2.bitwise_and(image, image, mask = mask)
